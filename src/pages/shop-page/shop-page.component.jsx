341a1344fc83213385f8f389029aa33c6f86521d
Don Jose Mathew <59858506+dontech09@users.noreply.github.com>
2021-04-25 20:58:08 +0530

    Redux

--- src/pages/shop-page/shop-page.component.jsx
+++ src/pages/shop-page/shop-page.component.jsx
@@ -1,24 +1,17 @@
 import React from "react";
-import SHOP_DATA from "./shop.data.js";
+import { SelectCollections } from "../../redux/shop/shop.selector";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 import CollectionPreview from "../../components/preview-collection/preview-collection.component";
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      collections: SHOP_DATA,
-    };
-  }
+const ShopPage = ({ collections }) => (
+  <div className="shop-page">
+    {collections.map(({ id, ...OtherCollectionProps }) => (
+      <CollectionPreview key={id} {...OtherCollectionProps} />
+    ))}
+  </div>
+);
 
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className="shop-page">
-        {collections.map(({ id, ...OtherCollectionProps }) => (
-          <CollectionPreview key={id} {...OtherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
-
-export default ShopPage;
+const mapStateToProps = createStructuredSelector({
+  collections: SelectCollections,
+});
+export default connect(mapStateToProps)(ShopPage);
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -2,7 +2,11 @@ import { combineReducers } from "redux";
 
 import userReducer from "./user/user.reducer";
 import cartreducer from "./cart/cart.reducer";
+import directoryReducer from "./directory/directory.reducer";
+import ShopReducer from "./shop/shop.reducer";
 export default combineReducers({
   user: userReducer,
   cart: cartreducer,
+  directory: directoryReducer,
+  shop: ShopReducer,
 });
